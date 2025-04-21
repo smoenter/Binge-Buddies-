7b55ac4ea81fa893233a509dce9d62dc8e4da800
alyssadailey <alyssadailey28@gmail.com>
2025-04-21 19:57:12 -0400

    rendered nav bar in header to ensure elements are inline

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { type MouseEvent} from 'react';
 import Auth from '../../utils/auth';
 import './index.css';
 import logoutIcon from '../../assets/logout.svg';
+import { Nav } from '../Nav'
 
 const Header = () => {
   const logout = (event: MouseEvent<HTMLButtonElement>) => {
@@ -10,29 +11,29 @@ const Header = () => {
     // Logs the user out by calling the logout method from Auth
     Auth.logout();
   };
+
+  const isLoggedIn = Auth.loggedIn();
+
   return (
-    <header className="custom-header text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
+    <header className="custom-header text-light mb-4 py-3">
+  <div className="container flex-row justify-space-between-lg justify-center align-center">
+    {isLoggedIn ? (
+      <div className="header-content flex-row align-center gap">
+        <Nav />
         <div className="auth-buttons">
-          {Auth.loggedIn() ? (
-  <>
-    <button className="btn btn-lg btn-light m-2" onClick={logout}>
-      <img src={logoutIcon} alt="Logout" style={{ width: '24px', height: '24px' }} />
-    </button>
-  </>
-) : (
-  <>
-    <Link className="auth-link" to="/login">
-      Login
-    </Link>
-    <Link className="auth-link" to="/signup">
-      Signup
-    </Link>
-  </>
-)}
+          <button className="btn btn-lg btn-light" onClick={logout}>
+            <img src={logoutIcon} alt="Logout" style={{ width: '24px', height: '24px' }} />
+          </button>
         </div>
       </div>
-    </header>
+    ) : (
+      <div className="auth-buttons">
+        <Link className="auth-link" to="/login">Login</Link>
+        <Link className="auth-link" to="/signup">Signup</Link>
+      </div>
+    )}
+  </div>
+</header>
   );
 };
 
