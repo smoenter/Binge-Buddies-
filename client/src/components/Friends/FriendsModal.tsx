03b1ccc777d8a015bab80bd713f9086b577f9869
smoenter <staceymoenter@gmail.com>
2025-04-26 12:26:17 -0400

    added search box but not currently working

--- client/src/components/Friends/FriendsModal.tsx
+++ client/src/components/Friends/FriendsModal.tsx
@@ -1,29 +1,46 @@
-import { useQuery } from "@apollo/client"
-import { QUERY_FRIENDS } from "../../utils/queries"
-import { useState } from "react"
+import { useQuery } from "@apollo/client";
+import { QUERY_FRIENDS } from "../../utils/queries";
+import { useState, useEffect } from "react";
 
-interface FriendsModalProp{
-    onClose: () => void
+interface FriendsModalProp {
+  onClose: () => void;
 }
 
-export default function FriendsModal({onClose}: FriendsModalProp) {
-  
-    const { data: friendsData, error: friendsError} = useQuery(QUERY_FRIENDS)
-    if (friendsError) {
-        console.log(JSON.stringify(friendsError))
-    }
-    const response= friendsData?.friends || []
-    const [FriendsList] = useState(() => response)
-  return (
-  <>
-  <h2>FRIENDS MODAL</h2>
-<ul>
-{FriendsList.map((obj:any)=>(
-    <li key={obj._id}>{obj.username}</li>
-))}
+export default function FriendsModal({ onClose }: FriendsModalProp) {
+  const { data: friendsData, error: friendsError } = useQuery(QUERY_FRIENDS);
+  const friends = friendsData?.friends || [];
 
-</ul>
-  <button onClick= {onClose}>ADD FRIEND</button>
-  </>
-  )
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredFriends, setFilteredFriends] = useState(friends);
+
+  useEffect(() => {
+    setFilteredFriends(
+      friends.filter((friend: any) =>
+        friend.username.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  }, [searchTerm, friends]);
+
+  if (friendsError) {
+    console.error(JSON.stringify(friendsError));
+  }
+
+  return (
+    <>
+      <h2>Find a Friend</h2>
+      <input
+        type="text"
+        placeholder="Search friends..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={{ marginBottom: "10px", padding: "5px" }}
+      />
+      <ul>
+        {filteredFriends.map((obj: any) => (
+          <li key={obj._id}>{obj.username}</li>
+        ))}
+      </ul>
+      <button onClick={onClose}>CLOSE</button>
+    </>
+  );
 }
