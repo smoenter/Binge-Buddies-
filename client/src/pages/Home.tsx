d68c8d8573ff6e76d0014f5a50962e2d3faedba7
alyssadailey <alyssadailey28@gmail.com>
2025-04-28 16:57:53 -0400

    added a filler BG video for sizing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,29 @@
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
+import VideoBackground from '../components/HomeVideoBG/index';
+import "./css/Home.css"
 
 const Home = () => {
   return (
+
+    <>
+      <VideoBackground />
+
     <main>
+      
       <div className="flex-row justify-center">
         {!Auth.loggedIn() ? (
+          // LOGGED OUT PAGE
           <div className="col-12 col-md-8 mb-3">
+
             <div className="card text-center">
-              {/* GETTING STARTED SECTION */}
+              {/* GETTING STARTED CARD SECTION */}
               <h4 className="card-header bg-dark text-light">Getting Started</h4>
               <div className="card-body">
                 {/* LOGIN/SIGNUP LINKS */}
                 <p>
-                  Please <Link to="/login">log in</Link> or{' '}
-                  <Link to="/signup">sign up</Link> to get started.
+                  Please <Link to="/login" className="auth-link">log in</Link> or{' '}
+                  <Link to="/signup" className="auth-link">sign up</Link> to get started.
                 </p>
               </div>
             </div>
@@ -29,6 +38,7 @@ const Home = () => {
                 </div>
           </div>
         ) : (
+          // LOGGED IN PAGE
           <div className="col-12 text-center">
             <h2>Welcome back, {Auth.getProfile().data.username}!</h2>
             <p>You're logged in and ready to go.</p>
@@ -36,6 +46,7 @@ const Home = () => {
         )}
       </div>
     </main>
+    </>
   );
 };
 
