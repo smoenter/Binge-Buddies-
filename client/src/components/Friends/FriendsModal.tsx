01d7f5c03d7e7876b458d5ecb1521ff92811d0f5
alyssadailey <alyssadailey28@gmail.com>
2025-05-02 16:25:06 -0400

    commented out handleSearch function- not using it

--- client/src/components/Friends/FriendsModal.tsx
+++ client/src/components/Friends/FriendsModal.tsx
@@ -47,20 +47,20 @@ export default function FriendsModal({ onClose }: FriendsModalProp) {
   }, [friendsData, currentFriendIds]);
 
   //Search for a friend by user
-  const handleSearch = () => {
-    const result = filteredFriends.filter((friend) =>
-      friend.username.toLowerCase() === searchTerm.toLowerCase()
-    );
-    if (result.length > 0) {
-      console.log("User found:", result[0]);
-      setMessage(`User found: ${result[0].username}`);
-      setMessageStyle({ color: "teal", fontWeight: "bold", marginTop: "10px" });
-    } else {
-      console.log("User not found");
-      setMessage("User not found");
-      setMessageStyle({ color: "red", fontWeight: "bold", marginTop: "10px" });
-    }
-  };
+  // const handleSearch = () => {
+  //   const result = filteredFriends.filter((friend) =>
+  //     friend.username.toLowerCase() === searchTerm.toLowerCase()
+  //   );
+  //   if (result.length > 0) {
+  //     console.log("User found:", result[0]);
+  //     setMessage(`User found: ${result[0].username}`);
+  //     setMessageStyle({ color: "teal", fontWeight: "bold", marginTop: "10px" });
+  //   } else {
+  //     console.log("User not found");
+  //     setMessage("User not found");
+  //     setMessageStyle({ color: "red", fontWeight: "bold", marginTop: "10px" });
+  //   }
+  // };
 
 
 //Toggle friend/unfriend status
