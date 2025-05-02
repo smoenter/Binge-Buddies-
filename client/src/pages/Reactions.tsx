591e417e5abf55e5f19fd01c83c1a6609b026b08
smoenter <staceymoenter@gmail.com>
2025-05-01 20:49:58 -0400

    thought form created

--- client/src/pages/Reactions.tsx
+++ client/src/pages/Reactions.tsx
@@ -1,6 +1,7 @@
 // src/pages/Reactions.tsx
 import PostCard from "../components/Post/index";
 import FriendsButton from "../components/Friends/index"
+import ThoughtForm from "../components/ThoughtForm";
 
 const Reactions = () => {
   return (
@@ -8,9 +9,10 @@ const Reactions = () => {
       <h1>💬 Reaction Dashboard</h1>
       {/* Friend search + add/unfriend UI here */}
       <div className="my-4">
+      <ThoughtForm/>
         {/* PostCard could include heart + comment functionality */}
-        <PostCard username="friend1" content="LOVED The Matrix!" />
-        <PostCard username="you" content="Omg Succession finale 💀" />
+        {/* <PostCard username="friend1" content="LOVED The Matrix!" /> */}
+        {/* <PostCard username="you" content="Omg Succession finale 💀" /> */}
       </div>
       <FriendsButton/>
    
