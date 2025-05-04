3ec4a3102c4eb3040af48cff307ee7ab72c415c7
alyssadailey <alyssadailey28@gmail.com>
2025-05-04 14:29:38 -0400

    created comment button with comment form

--- client/src/components/ReactionList.tsx/index.tsx
+++ client/src/components/ReactionList.tsx/index.tsx
@@ -1,10 +1,22 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { GET_REACTIONS } from '../../utils/queries';
+import CommentForm from '../CommentForm';
+
+import './index.css';
 
 const ReactionList = ({ mediaId }: { mediaId: string }) => {
-  const { loading: reactionLoading , error: reactionError, data } = useQuery(GET_REACTIONS, {
+
+  const { loading: reactionLoading, error: reactionError, data } = useQuery(GET_REACTIONS, {
     variables: { mediaId },
   });
+
+  const [activeCommentId, setActiveCommentId] = useState<string | null>(null);
+
+  const toggleCommentForm = (reactionId: string) => {
+    setActiveCommentId((prev) => (prev === reactionId ? null : reactionId));
+  };
+
 
   if (reactionLoading) return <p>Loading reactions...</p>;
   if (reactionError) return <p>Error loading reactions.</p>;
@@ -21,6 +33,17 @@ const ReactionList = ({ mediaId }: { mediaId: string }) => {
             <p>Season {r.season}, Episode {r.episode}</p>
             <p>Rating: {r.rating}</p>
             <small>{new Date(r.createdAt).toLocaleString()}</small>
+
+            <button onClick={() => toggleCommentForm(r._id)}>
+              <img
+                src="https://img.icons8.com/parakeet-line/48/speech-bubble-with-dots.png"
+                alt="comment icon"
+                style={{ width: '24px', height: '24px' }}
+              />
+            </button>
+
+            {activeCommentId === r._id && <CommentForm thoughtId={r._id} />}
+
           </div>
         ))
       )}
